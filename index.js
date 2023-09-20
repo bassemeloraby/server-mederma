6068917e42a7b112312b1ce486fc4d3bca3c32d6
bassem <basem12683@gmail.com>
2023-09-20 15:32:29 +0300

    get allDrugs url

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || '5000';
-
+const router = express.Router();
+const connectDB = require('./config/db');
 
 //middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+// main routes
+app.use('/api/allDrugs', require('./routes/allDrugsRoutes'));
+
 //test route
 app.get('/', (req, res) => {
   res.send('<h1>Hello World! . that is backend of mederma</h1>');
 });
 
-//listen to server
-app.listen(port, (err) => {
-    if (err) throw err;
-    console.log('Server listening on port'.red, port.yellow);
-  });
+//Connect to the database before listening
+connectDB().then(() => {//listen to server
+  app.listen(port, (err) => {
+      if (err) throw err;
+      console.log('Server listening on port'.red, port.yellow);
+    });})
+
 
