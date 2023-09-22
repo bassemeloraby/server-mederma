9c0751c092983d96ac062eafcd44cb70ae529ebe
bassem <basem12683@gmail.com>
2023-09-22 11:03:37 +0300

    ScientificDescriptionCodeRoot

--- controllers/allDrugsControllers.js
+++ controllers/allDrugsControllers.js
@@ -1,20 +1,24 @@
-const allDrugs = require('../models/allDrugsModel');
-const asyncHandler = require('express-async-handler');
-
+const allDrugs = require("../models/allDrugsModel");
+const asyncHandler = require("express-async-handler");
 
 // @desc    Get allDrugs{TradeName,ScientificName,PublicPrice}
 // @route   GET /api/drugs
 // @access  public
 const getAllDrugs = asyncHandler(async (req, res) => {
-    const allDrug = await allDrugs.find(
+  const allDrug = await allDrugs
+    .find(
       {},
-      { TradeName: 1, ScientificName: 1, PublicPrice: 1 }
-    ).sort({ TradeName: 1 });
-    res.status(200).json(allDrug);
-  });
+      {
+        TradeName: 1,
+        ScientificName: 1,
+        PublicPrice: 1,
+        ScientificDescriptionCodeRoot: 1,
+      }
+    )
+    .sort({ TradeName: 1 });
+  res.status(200).json(allDrug);
+});
 
-
-
-  module.exports = {
-    getAllDrugs,
-  };
+module.exports = {
+  getAllDrugs,
+};
