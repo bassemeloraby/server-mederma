3ca866a8f4857a66bb78fe143afc4803d2dee8f9
bassem <basem12683@gmail.com>
2024-03-22 14:22:01 +0300

    all data

--- controllers/allDrugsControllers.js
+++ controllers/allDrugsControllers.js
@@ -6,18 +6,7 @@ const asyncHandler = require("express-async-handler");
 // @access  public
 const getAllDrugs = asyncHandler(async (req, res) => {
   const allDrug = await allDrugs
-    .find(
-      {},
-      {
-        TradeName: 1,
-        ScientificName: 1,
-        PublicPrice: 1,
-        Strength: 1,
-        StrengthUnit: 1,
-        PharmaceuticalForm: 1,
-        ScientificDescriptionCodeRoot: 1,
-      }
-    )
+    .find()
     .sort({ TradeName: 1 });
   res.status(200).json(allDrug);
 });
