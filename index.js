a23277c3b7601f67ed08539ee99a29365e71e3db
bassem <basem12683@gmail.com>
2024-05-14 10:33:49 +0300

    3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const app = express();
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '3000';
 const router = express.Router();
 const connectDB = require('./config/db');
 
