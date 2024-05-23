9f1dbebd1d1c4ddd59ecb60198a9a5bfcecdd521
bassem <basem12683@gmail.com>
2024-05-23 10:50:55 +0300

    unique

--- models/specialArrayModel.js
+++ models/specialArrayModel.js
@@ -4,6 +4,7 @@ const specialArraySchema = mongoose.Schema({
   Description: {
     type: String,
     required: [true, "Please add a Description"],
+    unique: [true, "there is a list with same description"],
   },
   content: {
     type: Array,
