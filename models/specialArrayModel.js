4e534b4c3e26cd521951977179a3c9d6f754d3b2
bassem <basem12683@gmail.com>
2024-05-24 17:16:12 +0300

    timestamps

--- models/specialArrayModel.js
+++ models/specialArrayModel.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 
-const specialArraySchema = mongoose.Schema({
-  Description: {
-    type: String,
-    required: [true, "Please add a Description"],
+const specialArraySchema = mongoose.Schema(
+  {
+    Description: {
+      type: String,
+      required: [true, "Please add a Description"],
+    },
+    content: {
+      type: Array,
+      required: [true, "Please add a content"],
+    },
   },
-  content: {
-    type: Array,
-    required: [true, "Please add a content"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("specialArray", specialArraySchema);
