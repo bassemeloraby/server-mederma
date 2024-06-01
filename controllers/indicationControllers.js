911d93bcbb2b4244e4dfa4445bd8b5515bb96650
bassem <basem12683@gmail.com>
2024-06-01 08:24:38 +0300

    allIndication

--- controllers/indicationControllers.js
+++ controllers/indicationControllers.js
@@ -5,16 +5,33 @@ const asyncHandler = require("express-async-handler");
 // @route   GET /api/indications
 // @access  public
 const getAllIndications = asyncHandler(async (req, res) => {
-  const allIndication = await indication.find({},
-    {
-      INDICATION: 1,
-      ICD_10_CODE: 1,
-      SCIENTIFIC_NAME: 1,
-      PHARMACEUTICAL_FORM: 1,
-    });
-  res.status(200).json(allIndication);
+  if (req.query.scientificName) {
+    const indications = await indication.find(
+      { SCIENTIFIC_NAME: req.query.scientificName },
+      {
+        INDICATION: 1,
+        ICD_10_CODE: 1,
+        SCIENTIFIC_NAME: 1,
+        PHARMACEUTICAL_FORM: 1,
+      }
+    );
+    res.status(200).json(indications);
+    console.log(indications.length)
+  } else {
+    const allIndication = await indication.find(
+      {},
+      {
+        INDICATION: 1,
+        ICD_10_CODE: 1,
+        SCIENTIFIC_NAME: 1,
+        PHARMACEUTICAL_FORM: 1,
+      }
+    );
+    res.status(200).json(allIndication);
+    console.log(allIndication.length)
+  }
 });
 
 module.exports = {
-    getAllIndications,
+  getAllIndications,
 };
